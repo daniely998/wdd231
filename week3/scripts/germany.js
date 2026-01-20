c3fe39a46bb61aed30a47a72b0f9df3599df4a13
daniely998 <danzone998@gmail.com>
2026-01-20 12:59:06 +0800

    ???

--- week3/scripts/germany.js
+++ week3/scripts/germany.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat={49.75}&lon={6.64}&units=metric&appid={d51bd02fc76df6b1c37275628296cc7e}';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=metric&appid=d51bd02fc76df6b1c37275628296cc7e';
 
 async function apiFetch() {
     try {
@@ -10,12 +10,22 @@ async function apiFetch() {
         if (response.ok) {
             const data = await response.json();
             console.log(data);
+            displayResults(data);
         } else{
             throw Error(await response.text());
         }
     } catch (error) {
         console.log(error);
     }
+}
+
+function displayResults(data) {
+    let desc = data.weather[0].description;
+    captionDesc.textContent = `${desc}`;
+    currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    weatherIcon.setAttribute('SRC', iconsrc);
+    weatherIcon.setAttribute('alt', data.weather[0].description);
 }
 
 apiFetch();
