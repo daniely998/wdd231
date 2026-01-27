5948622dcce53dbd903e76dc3abe49c86762eb50
daniely998 <danzone998@gmail.com>
2026-01-27 13:07:15 +0800

    ?

--- week4/startLab/startLab/js/scripts.js
+++ week4/startLab/startLab/js/scripts.js
@@ -1,9 +1,21 @@
 const openButton = document.querySelector("#openButton");
 const dialogBox = document.querySelector("#dialogBox");
 const closeButton = document.querySelector("#closeButton");
+const dialogBoxTex = document.querySelector("#dialogBox div");
 
-openButton.addEventListener("click", () => {
+openButton1.addEventListener("click", () => {
     dialogBox.showModal();
+    dialogBoxTex.innerHTML = `One Apples contains 95 calories`;
+});
+
+openButton2.addEventListener("click", () => {
+    dialogBox.showModal();
+    dialogBoxTex.innerHTML = `C`;
+});
+
+openButton3.addEventListener("click", () => {
+    dialogBox.showModal();
+    dialogBoxTex.innerHTML = `D`;
 });
 
 closeButton.addEventListener("click", () => {
