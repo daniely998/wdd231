9a3a75a102a30fb31e0b456609a5c02f571b8a80
daniely998 <danzone998@gmail.com>
2026-01-29 19:40:07 +0800

    touch up

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,7 +1,9 @@
 const myInfo = new URLSearchParams(window.location.search);
 
 document.querySelector('#results'). innerHTML = `
-<p>Application for ${myInfo.get('first')} ${myInfo.get('last')}</p>
+<p>Applicant's Information</p>
+<p>First Name: ${myInfo.get('first')}</p>
+<p>Last Name: ${myInfo.get('last')}</p>
 <p>Email Address: ${myInfo.get('email')}</p>
 <p>Phone Number: ${myInfo.get('phone')}</p>
 <p>Organization Name: ${myInfo.get('org-name')}</p>
