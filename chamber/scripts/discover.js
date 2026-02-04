d68a214f2f1ce91de7f6fa31bc2b4a9d5b3cfc9e
daniely998 <danzone998@gmail.com>
2026-02-04 18:58:41 +0800

    fuck css

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,2 +1,24 @@
 import {places} from '../data/places.mjs';
-console.log(places)
+
+const display = document.querySelector('#places');
+
+function displayItems(data) {
+    data.forEach(place => {
+        let card = document.createElement('section');
+        card.className = 'places-card';
+        
+        card.innerHTML = `
+        <h2>${place.name}</h2>
+        <figure>
+            <img src="${place.photo_url}" alt="${place.name}" width=300 height=200 loading=lazy>
+        </figure>
+        <address>${place.address}</address>
+        <p>${place.description}</p>
+        <button>Learn more</button>
+        `;
+
+        display.appendChild(card);
+    })
+}
+
+displayItems(places)
